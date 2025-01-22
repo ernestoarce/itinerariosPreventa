b2c081c23c82f2fbc4f4cb180e2d5a12f22b61b6
ernesto <ernestoarce740@gmail.com>
2025-01-22 16:49:06 -0600

    Mejorar la interfaz de usuario: ajustar el diseño de los campos de entrada y optimizar la disposición de los elementos en la página principal.

--- index.php
+++ index.php
@@ -9,32 +9,32 @@
   <link rel="stylesheet" href="resources/toast.css">
 </head>
 <body>
-  <div id="content" v-cloak class="p-4">
-    <div class="row mb-3">
-
-      <div class="col d-flex justify-content-end">
-        <div class="input-group" style="width: 300px;">
-          <label class="input-group-text">ENTREGADOR:</label>
-          <select class="form-select" v-model="formItinerary.rute" @change="getClients()">
+  <div id="content" v-cloak class="container-fluid p-4">
+    <div class="container mb-3">
+    <div class="row mb-3 justify-content-center" style="max-width: 1000px;">
+      <div class="col-md-4 mb-2">
+        <div class="input-group">
+          <label class="input-group-text">Entregador:</label>
+          <select class="form-select" v-model="formItinerary.rute" @change="getClients">
             <option>TODOS</option>
-            <option v-for="v in virtualSellers">{{ v.TOUR_ID }}</option>
+            <option v-for="v in virtualSellers" :key="v.TOUR_ID">{{ v.TOUR_ID }}</option>
           </select>
         </div>
       </div>
 
-      <div class="col d-flex justify-content-center">
-        <div class="input-group" style="width: 400px;">
-          <label class="input-group-text">PREVENDEDOR:</label>
+      <div class="col-md-4 mb-2">
+        <div class="input-group">
+          <label class="input-group-text">Prevendedor:</label>
           <select class="form-select" v-model="filters.virtualManager">
             <option selected></option>
-            <option v-for="v in virtualSellers">{{ v.ID }}</option>
+            <option v-for="v in virtualSellers" :key="v.ID">{{ v.ID }}</option>
           </select>
         </div>
       </div>
 
-      <div class="col d-flex justify-content-start">
-        <div class="input-group" style="width: 300px;">
-          <label class="input-group-text">DÍA:</label>
+      <div class="col-md-4 mb-2">
+        <div class="input-group">
+          <label class="input-group-text">Día:</label>
           <select class="form-select" v-model="filters.day">
             <option selected></option>
             <option>LU</option>
@@ -46,78 +46,77 @@
           </select>
         </div>
       </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4">
+        <div class="input-group">
+          <input v-model="filters.text" type="text" class="form-control" placeholder="Filtrar por: Código/Nombre/Nom-Comercial/Ruta">
+        </div>
+      </div>
+      <div class="col-md-8 text-end">
+        <button class="btn btn-primary mx-2" disabled>
+          <i class="me-2 bi bi-geo-alt"></i>
+          Ordenar por distancia
+        </button>
+        <button class="btn btn-danger mx-2" disabled>
+          <i class="me-2 bi bi-save"></i>
+          Grabar Secuencia
+        </button>
+        <button class="btn btn-success mx-2" disabled>
+          <i class="me-2 bi bi-file-earmark-bar-graph"></i>
+          Graficar
+        </button>
+      </div>
+    </div>
 
     </div>
 
     <div class="row">
-      <div class="col">
-        <div class="input-group" style="width: 400px;">
-          <input v-model="filters.text" type="text" class="form-control" placeholder="Filtrar por: CODIGO/NOMBRE/NOM-COMERCIAL/RUTA">
-        </div>
-      </div>
-      <div class="col text-center" style="min-width: 700px;">
-        <button class="btn btn-sm btn-primary mx-2" disabled>
-          <i class="me-2 bi bi-geo-alt"></i>
-          ORDENAR POR DISTANCIA</button>
-        <button class="btn btn-sm btn-danger mx-2" disabled>
-          <i class="me-2 bi bi-save"></i>
-          GRABAR SECUENCIA</button>
-        <button class="btn btn-sm btn-success mx-2" disabled>
-          <i class="me-2 bi bi-file-earmark-bar-graph"></i>
-          GRAFICAR</button>
-      </div>
-      <div class="col text-end mb-3 fw-bold">Registros: {{ filteredClients.length }}</div>
+      <small class="col text-end fw-bold mx-2">Registros: {{ filteredClients.length }}</small>
     </div>
-    
-  
-    <div class="row px-2">
-    
-      <div class="col-12 px-1">
-        
-        <table class="table table-striped table-bordered table-sm mb-0">
-          <thead class="table" style="background-color:#0a58ca; color:white;">
-            <tr class="py-0">
-              <th scope="col" :colspan="showAllColumns ? 11 : 9" class="text-center py-0">
-                CLIENTES DE RUTAS DE PREVENTA
-              </th>
-              <th scope="col" colspan="7" class="text-center py-0">
-                DÍAS Y RUTA VIRTUAL QUE LE ATENDERÁ
-              </th>
+    <div class="row">
+      <div class="col-12">
+        <table class="table table-striped table-sm mb-0 table-hover">
+          <thead class="table text-white" style="background-color:rgb(9, 89, 175);">
+            <tr>
+              <th scope="col" :colspan="showAllColumns ? 11 : 9" class="text-center">Clientes de rutas de preventa</th>
+              <th scope="col" colspan="7" class="text-center">Días y ruta virtual que le atenderá</th>
             </tr>
             <tr>
-                <th scope="col" class="text-center py-0">
-                  ORDEN
-                    <button class="btn btn-sm p-0" @click="orderClients()">
-                      <i :class="sortOrder == '' ? 'bi bi-list text-white' : sortOrder == 'asc' ? 'bi bi-sort-alpha-down text-white' : 'bi bi-sort-alpha-up text-white'"></i>
-                    </button>
-                </th>
-              <th scope="col" class="text-center py-0">CODIGO</th>
-              <th scope="col" class="text-center py-0">NOMBRE</th>
-              <th scope="col" class="text-center py-0">NOM-COMERCIAL</th>
-              <th scope="col" class="text-center py-0">RUTA</th>
-              <th scope="col" class="text-center py-0">HORA DE VISITA</th>
-              <th scope="col" class="text-center py-0">TIEMPO DE VISITA</th>
-              <th scope="col" class="text-center py-0">TIEMPO DE TRASLADO</th>
-              <th scope="col" v-show="showAllColumns" class="text-center py-0">DIRECCION</th>
-              <th scope="col" v-show="showAllColumns" class="text-center py-0">TELEFONO</th>
-              <th scope="col" class="text-center py-0" style="width:5px;">
+              <th scope="col" class="text-center">
+                Orden
+                <button class="btn btn-sm p-0" @click="orderClients">
+                  <i :class="sortOrder === '' ? 'bi bi-list text-white' : sortOrder === 'asc' ? 'bi bi-sort-alpha-down text-white' : 'bi bi-sort-alpha-up text-white'"></i>
+                </button>
+              </th>
+              <th scope="col" class="text-center">Código</th>
+              <th scope="col" class="text-center">Nombre</th>
+                <th scope="col" class="text-center">Nom-Comercial</th>
+                <th scope="col" class="text-center">Ruta</th>
+                <th scope="col" class="text-center">Hora de Visita</th>
+                <th scope="col" class="text-center">Tiempo de Visita</th>
+                <th scope="col" class="text-center">Tiempo de Traslado</th>
+                <th scope="col" v-show="showAllColumns" class="text-center">Direccion</th>
+                <th scope="col" v-show="showAllColumns" class="text-center">Telefono</th>
+              <th scope="col" class="text-center">
                 <button class="btn btn-sm p-0" @click="showAllColumns = !showAllColumns">
                   <i :class="showAllColumns ? 'bi bi-caret-left-fill text-white' : 'bi bi-caret-right-fill text-white'"></i>
                 </button>
               </th>
-              <th scope="col" class="text-center py-0">PREVENDEDOR</th>
-              <th scope="col" class="text-center py-0">LU ({{ totals.lu }})</th>
-              <th scope="col" class="text-center py-0">MA ({{ totals.ma }})</th>
-              <th scope="col" class="text-center py-0">MI ({{ totals.mi }})</th>
-              <th scope="col" class="text-center py-0">JU ({{ totals.ju }})</th>
-              <th scope="col" class="text-center py-0">VI ({{ totals.vi }})</th>
-              <th scope="col" class="text-center py-0">SA ({{ totals.sa }})</th>
+              <th scope="col" class="text-center">Prevendedor</th>
+              <th scope="col" class="text-center">LU ({{ totals.lu }})</th>
+              <th scope="col" class="text-center">MA ({{ totals.ma }})</th>
+              <th scope="col" class="text-center">MI ({{ totals.mi }})</th>
+              <th scope="col" class="text-center">JU ({{ totals.ju }})</th>
+              <th scope="col" class="text-center">VI ({{ totals.vi }})</th>
+              <th scope="col" class="text-center">SA ({{ totals.sa }})</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(i, n) in filteredClients">
+            <tr v-for="(i, n) in filteredClients" :key="i.KUNNR">
               <td>
-                <select v-if="filters.day != '' && filters.virtualManager != '' && formItinerary.rute != 'TODOS' && formItinerary.rute != ''"
+                <select v-if="filters.day && filters.virtualManager && formItinerary.rute !== 'TODOS' && formItinerary.rute"
                 @change="setOrder(i, 'ORDEN_' + filters.day)"
                 class="py-0 form-select form-select-sm" v-model="i['ORDEN_' + filters.day]">
                   <option value="0"></option>
@@ -137,7 +136,7 @@
               <td class="text-center">
                 <select class="py-0 form-select form-select-sm" v-model="i.PREVENDEDOR" @change="setVirtualItinerary('PREVENDEDOR', i.KUNNR, i.PREVENDEDOR)">
                   <option></option>
-                  <option v-for="v in virtualSellers">{{ v.ID }}</option>
+                  <option v-for="v in virtualSellers" :key="v.ID">{{ v.ID }}</option>
                 </select>
               </td>
               <td class="text-center">
@@ -160,9 +159,7 @@
               </td>
             </tr>
             <tr v-if="filteredClients.length > 0">
-              <td :colspan="showAllColumns ? 12 : 10"
-              class="text-center py-0">TOTALES
-              </td>
+              <td :colspan="showAllColumns ? 12 : 10" class="text-center py-0">TOTALES</td>
               <td class="text-center py-0">{{ totals.lu }}</td>
               <td class="text-center py-0">{{ totals.ma }}</td>
               <td class="text-center py-0">{{ totals.mi }}</td>
@@ -172,25 +169,22 @@
             </tr>
           </tbody>
         </table>
-        <div v-show="loaders.list" colspan="8"
-        class="text-center m-2">
+        <div v-show="loaders.list" class="text-center m-2">
           <div class="spinner-border text-primary" role="status">
             <span class="visually-hidden">Loading...</span>
           </div>
         </div>
-        <div v-show="filteredClients.length == 0 && !loaders.list" class="alert alert-light text-center" role="alert">
+        <div v-show="filteredClients.length === 0 && !loaders.list" class="alert alert-light text-center" role="alert">
           No se encontraron resultados
         </div>
       </div>
     </div>
-
   </div>
 
+  <script src="resources/sw2.min.js"></script>
+  <script src="resources/vue26.js"></script>
+  <script src="resources/axios.min.js"></script>
+  <script src="index.js?t=<?= time(); ?>"></script>
+  <script src="resources/bootstrap.min.js"></script>
 </body>
-<script src="resources/sw2.min.js"></script>
-<script src="resources/vue26.js"></script>
-<script src="resources/axios.min.js"></script>
-<!-- <script src="resources/sweetalert.min.js"></script> -->
-<script src="index.js?t=<?= time(); ?>"></script>
-<script src="resources/bootstrap.min.js"></script>
 </html>
