aa61d979f742b8dc88bb5b3555536ea828163f90
ernesto <ernestoarce740@gmail.com>
2025-01-22 16:57:25 -0600

    No se realizaron cambios en el código.

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ var vue = new Vue({
     showAllColumns: false,
     virtualSellers: [],
     clients: [],
-    filters:{
+    filters: {
       text: '',
       virtualManager: '',
       day: '',
     },
-    formItinerary:{
+    formItinerary: {
       pass: 'getItinerary',
       rute: '',
     },
-    loaders:{
+    loaders: {
       list: false,
     },
-    totals:{
+    totals: {
       lu: 0,
       ma: 0,
       mi: 0,
@@ -27,251 +27,184 @@ var vue = new Vue({
     sortOrder: '',
     sap_api: '',
   },
-  mounted(){
-    this.getVirtualSellers()
+  mounted() {
+    this.getVirtualSellers();
   },
   computed: {
-    filteredClients: function(){
+    filteredClients() {
+      let tempClients = this.clients.filter(client => {
+        const text = this.filters.text.toLowerCase();
+        return (
+          client?.NAME1?.toLowerCase().includes(text) ||
+          client?.KUNNR?.toLowerCase().includes(text) ||
+          client?.NAME2?.toLowerCase().includes(text) ||
+          client?.LZONE?.toLowerCase().includes(text)
+        );
+      });
 
-      let tempClients = []
-      tempClients = this.clients.filter(client => {
-        return client?.NAME1?.toLowerCase().includes(this.filters.text.toLowerCase()) || 
-        client?.KUNNR?.toLowerCase().includes(this.filters.text.toLowerCase()) || 
-        client?.NAME2?.toLowerCase().includes(this.filters.text.toLowerCase()) || 
-         client?.LZONE?.toLowerCase().includes(this.filters.text.toLowerCase())  
-      })
-
-      tempClients = tempClients.filter(client => {
-        return client.PREVENDEDOR.includes(this.filters.virtualManager)
-      })
-
-
-      if(this.filters.day != ''){
-        tempClients = tempClients.filter(client => {
-          return client[this.filters.day]
-        })
+      if (this.filters.virtualManager) {
+        tempClients = tempClients.filter(client => client.PREVENDEDOR.includes(this.filters.virtualManager));
       }
 
-      // Totales
-      this.totals.lu = tempClients.filter(client => client.LU != '').length
-      this.totals.ma = tempClients.filter(client => client.MA != '').length
-      this.totals.mi = tempClients.filter(client => client.MI != '').length
-      this.totals.ju = tempClients.filter(client => client.JU != '').length
-      this.totals.vi = tempClients.filter(client => client.VI != '').length
-      this.totals.sa = tempClients.filter(client => client.SA != '').length
+      if (this.filters.day) {
+        tempClients = tempClients.filter(client => client[this.filters.day]);
+      }
 
-      //console.log(tempClients)
-      return tempClients
-    }
+      this.totals.lu = tempClients.filter(client => client.LU).length;
+      this.totals.ma = tempClients.filter(client => client.MA).length;
+      this.totals.mi = tempClients.filter(client => client.MI).length;
+      this.totals.ju = tempClients.filter(client => client.JU).length;
+      this.totals.vi = tempClients.filter(client => client.VI).length;
+      this.totals.sa = tempClients.filter(client => client.SA).length;
+
+      return tempClients;
+    },
   },
-  methods:{
-
-    getVirtualSellers: function(){
-      var config = {
-        method: 'get',
-        url: 'endpoint.php?pass=getVirtualSellers',
-        headers: { }
-      };
-
-      axios(config)
-      .then(function (response) {
-        console.log(response.data);
-        vue.virtualSellers = response.data[0]
-        vue.sap_api = vue.sap_api || response.data[1]
-      }).catch(function (error) {
-        console.log(error);
-      });
- 
+  methods: {
+    async getVirtualSellers() {
+      try {
+        const response = await axios.get('endpoint.php?pass=getVirtualSellers');
+        this.virtualSellers = response.data[0];
+        this.sap_api = this.sap_api || response.data[1];
+      } catch (error) {
+        console.error(error);
+      }
     },
-    // Consulta la lista de clientes desde SAP
-    getClients: function(){
+    async getClients() {
+      this.clients = [];
+      const ruta = this.formItinerary.rute;
+      const api = this.sap_api;
+      let url = '';
 
-       this.clients = []
-       const ruta = this.formItinerary.rute
-       let url = ''
-        
-       // GET URL FROM ENV
-       const api = this.sap_api
+      if (ruta === 'TODOS') {
+        url = `${api}/tablefs?table=KNA1&where=SORTL%20LIKE%20%27DET%%27OR%20SORTL%20LIKE%20%27DSM%%27&fields=KUNNR,NAME1,NAME2,SORTL,LZONE,STRAS,TELF1`;
+      } else {
+        url = `${api}/tablefs?table=KNA1&where=SORTL%20=%20%27${ruta}%27&fields=KUNNR,NAME1,NAME2,SORTL,LZONE,STRAS,TELF1`;
+      }
 
-       if (this.formItinerary.rute == 'TODOS') {
-        url = `${api}/tablefs?table=KNA1&where=SORTL%20LIKE%20%27DET%%27OR%20SORTL%20LIKE%20%27DSM%%27&fields=KUNNR,NAME1,NAME2,SORTL,LZONE,STRAS,TELF1`
-       } else {
-        url = `${api}/tablefs?table=KNA1&where=SORTL%20=%20%27${ruta}%27&fields=KUNNR,NAME1,NAME2,SORTL,LZONE,STRAS,TELF1`
-       }
-       //console.log(url)
-       
-       if(ruta != ''){
-         this.loaders.list = true
-         var config = {
-           method: 'get',
-           url: url,
-           headers: { }
-         };
-         axios(config)
-         .then(function (response) {
-           vue.loaders.list = false
-           vue.getVirtualItinerary(response.data)
-         })
-         .catch(function (error) {
-           vue.loaders.list = false
-           console.log(error);
-         });
-       }
-
-    },
-    // Consulta el itinerario actual de ORACLE
-    getVirtualItinerary: function(clients){
-        
-        const ruta = this.formItinerary.rute
-        const day = this.filters.day
-        const field = 'ORDEN_'+day
-
-        if(ruta != ''){
-  
-          this.loaders.list = true
-          var config = {
-            method: 'get',
-            url: `endpoint.php?pass=getItinerary&rute=${ruta}&orden=${field}`,
-            headers: { }
-          };
-
-          axios(config)
-          .then(function (response) {
-            console.log(response.data);
-            vue.loaders.list = false
-            vue.asignItineraries(clients, response.data)
-          })
-          .catch(function (error) {
-            vue.loaders.list = false
-            console.log(error);
-          });
-
+      if (ruta) {
+        this.loaders.list = true;
+        try {
+          const response = await axios.get(url);
+          this.loaders.list = false;
+          this.getVirtualItinerary(response.data);
+        } catch (error) {
+          this.loaders.list = false;
+          console.error(error);
         }
+      }
     },
-    setOrder(client, field){
-      const kunnr = client.KUNNR
-      const value = client[field]
+    async getVirtualItinerary(clients) {
+      const ruta = this.formItinerary.rute;
+      const day = this.filters.day;
+      const field = `ORDEN_${day}`;
 
-      axios.get(`endpoint.php?pass=setOrder&kunnr=${kunnr}&field=${field}&value=${value}`)
-      .then(response => {
-        if (response.data && response.data['exito'] && response.data['exito'] == 1) {
-          console.log(response.data)
+      if (ruta) {
+        this.loaders.list = true;
+        try {
+          const response = await axios.get(`endpoint.php?pass=getItinerary&rute=${ruta}&orden=${field}`);
+          this.loaders.list = false;
+          this.asignItineraries(clients, response.data);
+        } catch (error) {
+          this.loaders.list = false;
+          console.error(error);
+        }
+      }
+    },
+    async setOrder(client, field) {
+      const kunnr = client.KUNNR;
+      const value = client[field];
+
+      try {
+        const response = await axios.get(`endpoint.php?pass=setOrder&kunnr=${kunnr}&field=${field}&value=${value}`);
+        if (response.data && response.data.exito === 1) {
+          console.log(response.data);
         } else {
           Swal.fire({
             icon: 'error',
             title: 'Error',
-            text: 'No se pudo actualizar el orden para el cliente '+client.NAME1 + ' ' + client.NAME2,
-          })
-          this.getClients()
+            text: `No se pudo actualizar el orden para el cliente ${client.NAME1} ${client.NAME2}`,
+          });
+          this.getClients();
         }
-      })
-      .catch(error => {
-        console.log(error)
-      })
-    },
-    // Asigna los itinerarios a los clientes
-    asignItineraries: function(clients, itinerary){
-      clients.forEach(client => {
-
-        const temp = itinerary ? itinerary?.find(virtual => virtual.CODIGO == client.KUNNR) : null
-
-        // Asignar prevendedor
-        client['PREVENDEDOR'] = temp && temp['PREVENDEDOR'] ? temp['PREVENDEDOR'] : ''
-        
-        client['LU'] = temp && temp['LU'] ? parseInt(temp['LU']) : ''
-        client['MA'] = temp && temp['MA'] ? parseInt(temp['MA']) : ''
-        client['MI'] = temp && temp['MI'] ? parseInt(temp['MI']) : ''
-        client['JU'] = temp && temp['JU'] ? parseInt(temp['JU']) : ''
-        client['VI'] = temp && temp['VI'] ? parseInt(temp['VI']) : ''
-        client['SA'] = temp && temp['SA'] ? parseInt(temp['SA']) : ''
-
-        client['ORDEN_LU'] = temp && temp['ORDEN_LU'] ? temp['ORDEN_LU'] : ''
-        client['ORDEN_MA'] = temp && temp['ORDEN_MA'] ? temp['ORDEN_MA'] : ''
-        client['ORDEN_MI'] = temp && temp['ORDEN_MI'] ? temp['ORDEN_MI'] : ''
-        client['ORDEN_JU'] = temp && temp['ORDEN_JU'] ? temp['ORDEN_JU'] : ''
-        client['ORDEN_VI'] = temp && temp['ORDEN_VI'] ? temp['ORDEN_VI'] : ''
-        client['ORDEN_SA'] = temp && temp['ORDEN_SA'] ? temp['ORDEN_SA'] : ''
-      });
-
-      this.clients = clients
-    },
-    // Guarda un campo del itinerario virtual en ORACLE
-    setVirtualItinerary: function(day, code, value){
-
-      const ruta = this.formItinerary.rute
-      if(day && code && ruta != ''){
-
-        var config = {
-          method: 'get',
-          url: `endpoint.php?pass=setVirtualItinerary&day=${day}&code=${code}&virtualSeller=${value}&ruta=${ruta}`,
-          headers: { }
-        };
-  
-        axios(config)
-        .then(function (response) {
-          console.log(response.data);
-
-          if (response.data[0] && response.data[0].exito == 1) {
-            //vue.showToastSwal('success')
-          }else {
-            vue.clearDay(day, code)
-            vue.showToastSwal('error')
-          }
-
-        }).catch(function (error) {
-          console.log(error);
-        });
-
+      } catch (error) {
+        console.error(error);
       }
     },
-    clearDay: function(day, code){
-      let registry = this.clients.find(client => client.KUNNR == code)
-      registry[day] = ''
+    asignItineraries(clients, itinerary) {
+      clients.forEach(client => {
+        const temp = itinerary?.find(virtual => virtual.CODIGO === client.KUNNR) || {};
+
+        client.PREVENDEDOR = temp.PREVENDEDOR || '';
+        client.LU = temp.LU ? parseInt(temp.LU) : '';
+        client.MA = temp.MA ? parseInt(temp.MA) : '';
+        client.MI = temp.MI ? parseInt(temp.MI) : '';
+        client.JU = temp.JU ? parseInt(temp.JU) : '';
+        client.VI = temp.VI ? parseInt(temp.VI) : '';
+        client.SA = temp.SA ? parseInt(temp.SA) : '';
+
+        client.ORDEN_LU = temp.ORDEN_LU || '';
+        client.ORDEN_MA = temp.ORDEN_MA || '';
+        client.ORDEN_MI = temp.ORDEN_MI || '';
+        client.ORDEN_JU = temp.ORDEN_JU || '';
+        client.ORDEN_VI = temp.ORDEN_VI || '';
+        client.ORDEN_SA = temp.ORDEN_SA || '';
+      });
+
+      this.clients = clients;
     },
-    showToastSwal: function(type){
+    async setVirtualItinerary(day, code, value) {
+      const ruta = this.formItinerary.rute;
+      if (day && code && ruta) {
+        try {
+          const response = await axios.get(`endpoint.php?pass=setVirtualItinerary&day=${day}&code=${code}&virtualSeller=${value}&ruta=${ruta}`);
+          if (response.data[0]?.exito === 1) {
+            //vue.showToastSwal('success')
+          } else {
+            this.clearDay(day, code);
+            this.showToastSwal('error');
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    },
+    clearDay(day, code) {
+      const registry = this.clients.find(client => client.KUNNR === code);
+      if (registry) {
+        registry[day] = '';
+      }
+    },
+    showToastSwal(type) {
       const Toast = Swal.mixin({
         toast: true,
         position: 'top-right',
         iconColor: 'white',
         customClass: {
-          popup: 'colored-toast'
+          popup: 'colored-toast',
         },
         showConfirmButton: false,
         timer: 1500,
-        timerProgressBar: true
-      })
+        timerProgressBar: true,
+      });
 
-      if(type == 'success'){
-        Toast.fire({
-          icon: 'success',
-          title: 'Hecho!'
-        })
-      } else if (type == 'error'){
-        Toast.fire({
-          icon: 'error',
-          title: 'Error!'
-        })
-      }
-
+      Toast.fire({
+        icon: type,
+        title: type === 'success' ? 'Hecho!' : 'Error!',
+      });
     },
-    orderClients: function(){
-      const day = this.filters.day
-      const field = 'ORDEN_'+day
-      this.sortOrder = this.sortOrder == 'asc' ? 'desc' : 'asc'
+    orderClients() {
+      const day = this.filters.day;
+      const field = `ORDEN_${day}`;
+      this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
 
-      if (!day || day == '' && !this.filters.virtualManager) {
-        return
+      if (!day && !this.filters.virtualManager) {
+        return;
       }
 
-      this.clients = this.clients.sort((a, b) => {
-        if(this.sortOrder == 'asc'){
-          return a[field] - b[field]
-        } else {
-          return b[field] - a[field]
-        }
-      })
-    }
-
-  }
+      this.clients.sort((a, b) => {
+        return this.sortOrder === 'asc' ? a[field] - b[field] : b[field] - a[field];
+      });
+    },
+  },
 });
-
