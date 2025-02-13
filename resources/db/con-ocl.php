f6147913a41f6bd89811dde8e197e7e61d4bfd43
ernesto <ernestoarce740@gmail.com>
2025-02-13 15:38:38 -0600

    Refactor la conexión a la base de datos para usar variables de entorno en con-ocl.php y eliminar credenciales hardcodeadas en credenciales.php.

--- resources/db/con-ocl.php
+++ resources/db/con-ocl.php
@@ -5,8 +5,11 @@ include 'PHPRequest.php';
 include 'APIRequest.php';
 include 'credenciales.php';
 
+$ruta = getenv('ROUTE');
+$usuario = getenv('USER');
+$password = getenv('PASS');
+
 $ora = new PHPRequest($ruta,$usuario,$password);
 // $ora = new APIRequest();
-
 
 ?>
--- resources/db/credenciales.php
+++ resources/db/credenciales.php
@@ -1,13 +1,3 @@
 <?php 
 
-//SV
-$usuario="HH";
-$password="HHLACTOSA";
-$ruta="oci:dbname=//10.10.4.9:1521/lactosa";
-
-// FROM ENV
-// $usuario = getenv('USER');
-// $password = getenv('PASS');
-// $ruta = getenv('ROUTE');
-
 ?>
